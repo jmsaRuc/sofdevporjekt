c2486b1bbed05f9b075d78b98bdc7d43c6eba5ca
jmsaRuc <jmsa@ruc.dk>
2023-10-31 12:39:48 +0100

    update

--- bookshipment/src/main/java/portfolio/projekt2/controllers/gui.java
+++ bookshipment/src/main/java/portfolio/projekt2/controllers/gui.java
@@ -6,3 +6,17 @@ public class gui {
 
 
 // https://www.maersk.com/schedules/pointToPoint
+
+// type in city name from
+
+
+//type in city name to 
+
+//date type in departing or arraiveing
+
+//container type amount
+
+//press search 
+
+
+//when press search tjek at date is a date, and show a tabel of vesses comming from a function, 
